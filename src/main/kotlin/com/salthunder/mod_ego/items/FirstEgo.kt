44ba6192441e016ebdc037a144422b26a8e4ed81
Lucas Salgado <lssalgado>
2022-02-05 19:54:10 -0300

    Using ServerPlayerEntity instead of client.

--- src/main/kotlin/com/salthunder/mod_ego/items/FirstEgo.kt
+++ src/main/kotlin/com/salthunder/mod_ego/items/FirstEgo.kt
@@ -9,6 +9,7 @@ import net.minecraft.entity.effect.StatusEffects
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.item.Item
 import net.minecraft.item.ItemStack
+import net.minecraft.server.network.ServerPlayerEntity
 import net.minecraft.text.Text
 import net.minecraft.util.Hand
 import net.minecraft.util.TypedActionResult
@@ -20,26 +21,28 @@ class FirstEgo(settings: Settings): Item(settings) {
 
     override fun use(world: World?, user: PlayerEntity, hand: Hand?): TypedActionResult<ItemStack> {
 
-        if (hand == Hand.MAIN_HAND) {
-            user.sendMessage(Text.of("Hand = '${hand.toString()}'"), false)
-//            user.addStatusEffect(
-//                StatusEffectInstance(SpeedEffect(), 3000, 5)
-//            )
-            val multimapBuilder = ImmutableMultimap.builder<EntityAttribute, EntityAttributeModifier>()
+        if (user is ServerPlayerEntity) {
+            if (hand == Hand.MAIN_HAND) {
+                user.sendMessage(Text.of("Hand = '$hand'"), false)
+                val multimapBuilder = ImmutableMultimap.builder<EntityAttribute, EntityAttributeModifier>()
 
-            multimapBuilder.put(EntityAttributes.GENERIC_MOVEMENT_SPEED, EntityAttributeModifier("SPEEEEED", 0.2, EntityAttributeModifier.Operation.MULTIPLY_TOTAL))
+                multimapBuilder.put(
+                    EntityAttributes.GENERIC_MOVEMENT_SPEED,
+                    EntityAttributeModifier("SPEEEEED", 0.2, EntityAttributeModifier.Operation.MULTIPLY_TOTAL)
+                )
 
-            user.attributes.addTemporaryModifiers(multimapBuilder.build())
+                user.attributes.addTemporaryModifiers(multimapBuilder.build())
 
-            user.sendMessage(Text.of("MS = '${user.movementSpeed}'"), false)
-            user.sendMessage(Text.of("MS = '${user.movementSpeed}'"), false)
-        } else {
-            user.sendMessage(Text.of("Hand = '${hand.toString()}'"), false)
-            user.addStatusEffect(
-                StatusEffectInstance(StatusEffects.SPEED, 3000, 5)
-            )
-            user.sendMessage(Text.of("MS = '${user.movementSpeed}'"), false)
-            user.sendMessage(Text.of("MS = '${user.movementSpeed}'"), false)
+                user.sendMessage(Text.of("MS = '${user.movementSpeed}'"), false)
+                user.sendMessage(Text.of("MS = '${user.movementSpeed}'"), false)
+            } else {
+                user.sendMessage(Text.of("Hand = '${hand.toString()}'"), false)
+                user.addStatusEffect(
+                    StatusEffectInstance(StatusEffects.SPEED, 3000, 5)
+                )
+                user.sendMessage(Text.of("MS = '${user.movementSpeed}'"), false)
+                user.sendMessage(Text.of("MS = '${user.movementSpeed}'"), false)
+            }
         }
         return TypedActionResult.success(user.getStackInHand(hand))
     }
