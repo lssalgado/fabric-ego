df4a558aaa9d0af5a9d2e1f4193f2cd82c9fcccd
Lucas Salgado <lssalgado>
2022-02-05 20:49:09 -0300

    Adding behavior based on stat counter.

--- src/main/kotlin/com/salthunder/mod_ego/items/EgoPieces.kt
+++ src/main/kotlin/com/salthunder/mod_ego/items/EgoPieces.kt
@@ -1,13 +1,24 @@
 package com.salthunder.mod_ego.items
 
+import com.salthunder.mod_ego.statistics.EgoStatisticIdentifiers
+import net.minecraft.item.ItemStack
+import net.minecraft.item.Items.GOLDEN_APPLE
 import net.minecraft.server.network.ServerPlayerEntity
+import net.minecraft.stat.Stats
 import net.minecraft.text.Text
 
 object EgoPieces {
     val ZOMBIE_EGO = PieceOfEgo(EgoFoodComponents.ZOMBIE_EGO, 1) { livingEntity ->
         if (livingEntity is ServerPlayerEntity) {
-            //TODO add statistic
             livingEntity.sendMessage(Text.of("Extra action :D"), false)
+
+            /**TODO move to [PieceOfEgo] and improve this code*/
+            val consumed =
+                livingEntity.statHandler.getStat(Stats.CUSTOM, EgoStatisticIdentifiers.EGO_CONSUMPTION.identifier)
+            livingEntity.sendMessage(Text.of("Consumed: '$consumed'"), false)
+            if (consumed % 5 == 0) {
+                livingEntity.giveItemStack(ItemStack({ GOLDEN_APPLE }, 1))
+            }
         }
     }
 }
