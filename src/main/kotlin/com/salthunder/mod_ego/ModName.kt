3347b9847e1d13c06a9d01c240bf8ebd1f665c26
Lucas Salgado <lssalgado>
2022-02-05 19:53:50 -0300

    Cleaning initialization code.

--- src/main/kotlin/com/salthunder/mod_ego/ModName.kt
+++ src/main/kotlin/com/salthunder/mod_ego/ModName.kt
@@ -1,12 +1,12 @@
 package com.salthunder.mod_ego
 
+import com.salthunder.mod_ego.items.EgoPieces
 import com.salthunder.mod_ego.items.FirstEgo
 import com.salthunder.mod_ego.statuseffects.SpeedEffect
 import net.fabricmc.api.ModInitializer
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings
 import net.minecraft.block.Blocks
-import net.minecraft.item.Item
 import net.minecraft.item.ItemGroup
 import net.minecraft.item.ItemStack
 import net.minecraft.item.Items
@@ -18,16 +18,38 @@ import net.minecraft.util.registry.Registry
 object ModName : ModInitializer {
     private const val mod_ego = "mod_ego"
 
-    val ITEM_GROUP = FabricItemGroupBuilder.build(Identifier(mod_ego, "general")) { ItemStack(Blocks.COBBLESTONE) }
-    val OTHER_GROUP = FabricItemGroupBuilder.build(Identifier(mod_ego, "other")) { ItemStack(Items.BOWL) }
+    private val ITEM_GROUP = FabricItemGroupBuilder.build(Identifier(mod_ego, "general")) { ItemStack(Blocks.COBBLESTONE) }
+    val FOOD_GROUP: ItemGroup = FabricItemGroupBuilder.build(Identifier(mod_ego, "foods")) { ItemStack(Items.BOWL) }
 
-    val EGO_SPEED = FirstEgo(FabricItemSettings().group(ITEM_GROUP))
+    private val EGO_SPEED = FirstEgo(FabricItemSettings().group(ITEM_GROUP))
 
-    val SPEED = SpeedEffect()
+    private val SPEED = SpeedEffect()
 
     override fun onInitialize() {
         println("Example mod has been initialized.")
-        Registry.register(Registry.ITEM, Identifier("ego", "speed_item"), EGO_SPEED)
+        registerItems()
+        registerTools()
+        registerPieces()
+        registerStatus()
+    }
+
+    private fun registerStatus() {
         Registry.register(Registry.STATUS_EFFECT, Identifier("ego", "speed"), SPEED)
+    }
+
+    private fun registerPieces() {
+        Registry.register(
+            Registry.ITEM,
+            Identifier("ego", "zombie_ego"),
+            EgoPieces.ZOMBIE_EGO
+        )
+    }
+
+    private fun registerTools() {
+
+    }
+
+    private fun registerItems() {
+        Registry.register(Registry.ITEM, Identifier("ego", "speed_item"), EGO_SPEED)
     }
 }
