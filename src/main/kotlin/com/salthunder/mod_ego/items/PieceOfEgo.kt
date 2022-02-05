378214e0217585696f234fdb31e0c82910aa988f
Lucas Salgado <lssalgado>
2022-02-05 20:27:04 -0300

    Adding the value to be added to PieceOfEgo constructor.

--- src/main/kotlin/com/salthunder/mod_ego/items/PieceOfEgo.kt
+++ src/main/kotlin/com/salthunder/mod_ego/items/PieceOfEgo.kt
@@ -1,6 +1,7 @@
 package com.salthunder.mod_ego.items
 
 import com.salthunder.mod_ego.Ego.FOOD_GROUP
+import com.salthunder.mod_ego.statistics.EgoStatisticIdentifiers
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings
 import net.minecraft.entity.LivingEntity
 import net.minecraft.item.FoodComponent
@@ -9,9 +10,10 @@ import net.minecraft.item.ItemStack
 import net.minecraft.server.network.ServerPlayerEntity
 import net.minecraft.world.World
 
-class PieceOfEgo(foodComponent: FoodComponent, private val extraEffects: (LivingEntity) -> Unit): Item(FabricItemSettings().food(foodComponent).group(FOOD_GROUP)) {
+class PieceOfEgo(foodComponent: FoodComponent, private val value: Int, private val extraEffects: (LivingEntity) -> Unit): Item(FabricItemSettings().food(foodComponent).group(FOOD_GROUP)) {
     override fun finishUsing(stack: ItemStack?, world: World?, user: LivingEntity?): ItemStack {
         if (user is ServerPlayerEntity) {
+            user.increaseStat(EgoStatisticIdentifiers.EGO_CONSUMPTION.identifier, value)
             extraEffects(user)
         }
         return super.finishUsing(stack, world, user)
